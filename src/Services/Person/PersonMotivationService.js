01cf9602cd7a03da628d09e28a30fab36929d730
isakh <rafiisakh@gmail.com>
2017-02-20 15:33:30 +0700

    #39 - Rewrite PersonMotivation service

--- src/Services/Person/PersonMotivationService.js
+++ src/Services/Person/PersonMotivationService.js
@@ -1,57 +1,42 @@
-require ('lodash')
+const knex = require('../../connection')
 
 class PersonMotivationService {
 
-	constructor(PersonMotivationRepositoryInterface) {
-		this.repository = PersonMotivationRepositoryInterface
+	constructor() {
+		this.tablename = 'person_motivations'
 	}
 
-	create(motivation) {
-		return this.repository.create(motivation)
+	browse() {
+		return knex
+			.select()
+			.from(this.tablename)
+			.get()
 	}
 
-	update(motivation) {
-		return this.repository.update(motivation)
+	read(id) {
+		return knex(this.tablename)
+			.where('id', id)
+			.first()
 	}
 
-	delete(motivation) {
-		return this.repository.delete(motivation)
+	edit(motivation) {
+		return knex(this.tablename)
+			.where('id', motivation.id)
+			.update(motivation)
 	}
 
-	sync(person, ...motivations) {
-		const compareKey = ['getMotivation.getKey']
-
-		const toSync = _.map(motivations, motivation => {
-			return new PersonMotivation().setPerson(person).setMotivation(motivation)
-		})
-		const currents = this.repository.getFor(person)
-
-		const toDelete = currents.filterNoExistIn($toSync, compareKey)
-		const noChange = currents.filterNoExistIn(toDelete, ['getKey'])
-		const toAdd = toSync.filterNoExistIn(noChange, compareKey)
-
-		const result = {
-			deleted : 0,
-			added : 0
-		}
-
-		if(toDelete.count > 0) {
-			const ids = ._reduce(toDelete, function(carry, personMotivation)) {
-				carry.push(personMotivation.getKey())
-				return carry
-			}, [])
-			
-			deleted = this.deleteMultiple(ids)
-			result.deleted = deleted.delete_count
-		}
-
-		toAdd.map(function(instance){
-			this.create(instance)
-		})
-		
-		result.added = toAdd.count()
-
-		return result
+	add(motivation) {
+		return knex(this.tablename)
+			.insert(motivation)
 
 	}
+
+	delete(id) {
+		return knex(this.tablename)
+			.where('id', id)
+			.delete()
+	}
+
 }
+
+module.exports = PersonMotivationService
