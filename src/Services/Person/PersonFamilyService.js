b8a1d3780af7fc2e98f368a584ba84b4ce28f543
erdivartanovich <erdivartanovich@gmail.com>
2017-02-20 21:20:43 +0700

    #35 add inherit construct

--- src/Services/Person/PersonFamilyService.js
+++ src/Services/Person/PersonFamilyService.js
@@ -7,6 +7,7 @@ class PersonFamilyService extends BaseService {
      * Constructor
      */
     constructor() {
+        super()
         //set the table name
         this.tableName = 'person_family'
     }
