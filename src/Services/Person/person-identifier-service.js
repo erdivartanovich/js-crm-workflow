f52ac92b3a25770e117d4f0b9a2d28206f5c1488
erdivartanovich <erdivartanovich@gmail.com>
2017-02-20 18:51:47 +0700

    #36 rewrite person identifier add timestamp operation

--- src/Services/Person/person-identifier-service.js
+++ src/Services/Person/person-identifier-service.js
@@ -1,5 +1,12 @@
 const knex = require('../../connection')
 
+const moment = require('moment')
+const DATEFORMAT = 'YYYY-MM-DD HH:mm:ss'
+
+//Empty entity object
+var entity = {}
+
+
 class PersonIdentifierService {
 
     constructor() {
@@ -19,20 +26,40 @@ class PersonIdentifierService {
     }
 
     edit(personIdentifier) {
+        entity = this.editTimestamp(personIdentifier)
         return knex(this.tableName)         
-            .where('id', personIdentifier.id)
-            .update(personIdentifier)
+            .where('id', entity.id)
+            .update(entity)
     }
 
     add(personIdentifier) {
+        entity = this.addTimestamp(personIdentifier)
         return knex(this.tableName)         
-            .insert(personIdentifier)
+            .insert(entity)
     }
 
     delete(id) {
         return knex(this.tableName)   
             .where('id', id)      
             .del()
+    }
+
+    addTimestamp(payload) {
+        if (! payload.created_at) {
+            payload.created_at = (new moment).format(DATEFORMAT)
+        }
+
+        payload = this.editTimestamp(payload)
+
+        return payload
+    }
+
+    editTimestamp(payload) {
+        if (! payload.updated_at) {
+            payload.updated_at = (new moment).format(DATEFORMAT)
+        }
+
+        return payload
     }
 }
 
