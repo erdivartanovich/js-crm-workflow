ee961b0b8a73d82ea5d508f14e003f11c8c53b78
kris <kristoforusrp@gmail.com>
2017-02-20 21:28:59 +0700

    #9 Edited leadType service using BaseService

--- src/Services/Person/LeadTypeService.js
+++ src/Services/Person/LeadTypeService.js
@@ -5,33 +5,7 @@ class LeadTypeService {
   constructor() {
     this.tableName = 'lead_types'
   }
-
-  browse() {
-    return knex(this.tableName)
-  }
-
-  read(id) {
-    return knex(this.tableName)
-      .where('id', id)
-      .first()
-  }
-
-  edit(leadType) {
-    return knex(this.tableName)
-      .where('id', leadType.id)
-      .update(leadType)
-  }
-
-  add(leadType) {
-    return knex(this.tableName)
-      .insert(leadType)
-  }
-
-  delete(id) {
-    return knex(this.tableName)
-    .where('id', id)
-    .del()
-  }
+  
 }
 
 
