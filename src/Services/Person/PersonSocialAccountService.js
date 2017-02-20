7a8cf4fda985a0eff558fbd00eea2dfb9c560257
hystolytc <melife4@gmail.com>
2017-02-20 21:22:57 +0700

    #43 - extends  BaseService in PersonSocialAccount

--- src/Services/Person/PersonSocialAccountService.js
+++ src/Services/Person/PersonSocialAccountService.js
@@ -1,37 +1,13 @@
 const knex = require('../../connection')
+const BaseService = require('../BaseService')
 
-class PersonSocialAccountService{
+class PersonSocialAccountService extends BaseService{
+
     constructor(){
+        super()
         this.tableName = 'person_social_accounts'
-        this.id = 'id'
     }
 
-    browse(){
-        return knex(this.tableName)
-    }
-
-    read(id){
-        return knex
-              .select()
-              .from(this.tableName)
-              .where(this.id, id)
-              .first()
-    }
-
-    edit(personSocialAccount){
-        return knex(this.tableName)
-               .where(this.id, personSocialAccount.id)
-               .update(personSocialAccount)
-    }
-
-    add(personSocialAccount){
-        return knex(this.tableName).insert(personSocialAccount)
-    }
-
-    delete(id){
-        return knex(this.tableName).where(this.id, id).del()
-    }
 }
-
 
 module.exports = PersonSocialAccountService
