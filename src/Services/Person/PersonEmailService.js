a4c82c794faedb9ce051f2a19458274315aa5e81
Syamsul Anwar <msyamsulanwr@gmail.com>
2017-02-20 21:20:20 +0700

    #34 fix Person Email Service

--- src/Services/Person/PersonEmailService.js
+++ src/Services/Person/PersonEmailService.js
@@ -1,58 +1,12 @@
-const knex = require('../../connection')
+const BaseService = require('../BaseService')
 
 
-
-class PersonEmailService {
-
-
-
+class PersonEmailService extends BaseService {
     constructor() {
+      super()
         this.tableName = 'person_emails'
-        this.id = `id`
 
     }
-
-
-    // browse success
-    browse() {
-
-        return knex(this.tableName)
-    }
-
-
-    // read success
-    read(id) {
-
-        return knex.select().from(this.tableName).where(this.id, id).first()
-
-    }
-
-
-    // edit success
-    edit(email) {
-
-      return knex(this.tableName).where(this.id, email.id).update(email)
-
-  }
-
-
-    // add success
-    add(email) {
-
-      return knex(this.tableName).insert(email)
-
-  }
-
-
-    // delete success
-    delete(id) {
-
-      return knex(this.tableName).where(this.id, id).del()
-
-  }
-
 }
-
-
 
 module.exports = PersonEmailService
