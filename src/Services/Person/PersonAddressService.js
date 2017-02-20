39df234c2134356f2fffe8e3f7120de99f3556fc
hystolytc <melife4@gmail.com>
2017-02-20 21:59:22 +0700

    #12 - extends BaseService in PersonAddressService

--- src/Services/Person/PersonAddressService.js
+++ src/Services/Person/PersonAddressService.js
@@ -1,37 +1,13 @@
 const knex = require('../../connection')
+const BaseService = require('../BaseService')
 
-class PersonAddressService{
+class PersonAddressService extends BaseService{
 
     constructor() {
+        super()
         this.tableName = 'person_addresses'
-        this.id = 'id'
     }
 
-    browse() {
-        return knex(this.tableName)
-    }
-
-    read(id) {
-        return knex
-            .select()
-            .from(this.tableName)
-            .where(this.id, id)
-            .first()
-    }
-
-    edit(personAddress) {
-        return knex(this.tableName)
-               .where(this.id, personAddress.id)
-               .update(person)
-    }
-
-    add(personAddress) {
-      return knex(this.tableName).insert(personAddress)
-    }
-
-    delete(id) {
-      return knex(this.tableName).where(this.id, id).del()
-    }
 }
 
 module.exports = PersonAddressService
