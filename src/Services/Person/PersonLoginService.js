30cc0bd5384c371d2162be81d615f6b785c1439a
isakh <rafiisakh@gmail.com>
2017-02-20 20:42:49 +0700

    #37 - Add BaseService file

--- src/Services/Person/PersonLoginService.js
+++ src/Services/Person/PersonLoginService.js
@@ -1,42 +1,12 @@
+const BaseService = require('../BaseService')
 const knex = require('../../connection')
 
-class PersonLoginService {
+class PersonLoginService extends BaseService{
 
 	constructor() {
-		this.tablename = 'person_logins'
+		super()
+		this.tableName = 'person_logins'
 	}
-
-	browse() {
-		return knex
-			.select()
-			.from(this.tablename)
-			.get()
-	}
-
-	read(id) {
-		return knex(this.tablename)
-			.where('id', id)
-			.first()
-	}
-
-	edit(login) {
-		return knex(this.tablename)
-			.where('id', login.id)
-			.update(login)
-	}
-
-	add(login) {
-		return knex(this.tablename)
-			.insert(login)
-
-	}
-
-	delete(id) {
-		return knex(this.tablename)
-			.where('id', id)
-			.delete()
-	}
-
 }
 
 module.exports = PersonLoginService
