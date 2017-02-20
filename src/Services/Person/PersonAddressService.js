5703cc11809b6f9cd698bb6cf08efef8b91b1c60
hystolytc <melife4@gmail.com>
2017-02-20 16:51:27 +0700

    #12 Rewrite PersonAddressService

--- src/Services/Person/PersonAddressService.js
+++ src/Services/Person/PersonAddressService.js
@@ -4,37 +4,32 @@ class PersonAddressService{
 
     constructor() {
         this.tableName = 'person_addresses'
+        this.identifier = 'id'
     }
 
     browse() {
-        return knex
-            .select()
-            .from(this.tableName)
-            .get()
+        return knex(this.tableName)
     }
 
     read(id) {
         return knex
             .select()
             .from(this.tableName)
-            .where('id', id)
+            .where(this.identifier, id)
             .first()
     }
 
     edit(person) {
-        return knex
-            .from(this.tableName)
-            .where('person_id', person.person_id)
-            .update(person)
+        return knex(this.tableName).
+               where(this.identifier, person.id).update(person)
     }
 
-
-    add() {
-
+    add(person) {
+      return knex(this.tableName).insert(person)
     }
 
-    delete() {
-
+    delete(id) {
+      return knex(this.tableName).where(this.identifier,id).del()
     }
 }
 
