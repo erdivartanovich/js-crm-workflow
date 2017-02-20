7c15834e0ce840f674d99e59c5bd4ae7560876d9
erdivartanovich <erdivartanovich@gmail.com>
2017-02-20 18:56:08 +0700

    #41 refactor person's score add timestamp operation

--- src/Services/Person/person-scores-service.js
+++ src/Services/Person/person-scores-service.js
@@ -1,5 +1,11 @@
 const knex = require('../../connection')
 
+const moment = require('moment')
+const DATEFORMAT = 'YYYY-MM-DD HH:mm:ss'
+
+//Empty entity object
+var entity = {}
+
 class PersonScoresService {
 
     constructor() {
@@ -19,20 +25,40 @@ class PersonScoresService {
     }
 
     edit(personScore) {
+        entity = this.editTimestamp(personScore)
         return knex(this.tableName)         
-            .where('id', personScore.id)
-            .update(personScore)
+            .where('id', entity.id)
+            .update(entity)
     }
 
     add(personScore) {
+        entity = this.addTimestamp(personScore)
         return knex(this.tableName)         
-            .insert(personScore)
+            .insert(entity)
     }
 
     delete(id) {
         return knex(this.tableName)   
             .where('id', id)      
             .del()
+    }
+
+    addTimestamp(payload) {
+        if (! payload.created_at) {
+            payload.created_at = (new moment).format(DATEFORMAT)
+        }
+
+        payload = this.editTimestamp(payload)
+
+        return payload
+    }
+
+    editTimestamp(payload) {
+        if (! payload.updated_at) {
+            payload.updated_at = (new moment).format(DATEFORMAT)
+        }
+
+        return payload
     }
 }
 
