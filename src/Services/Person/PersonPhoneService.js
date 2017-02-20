0f78a82f5873f66778ef3bae9416de638b88c791
Pholenk <pholenkadi17@gmail.com>
2017-02-20 21:19:38 +0700

    #32 - fixing Person Phone Service

--- src/Services/Person/PersonPhoneService.js
+++ src/Services/Person/PersonPhoneService.js
@@ -1,37 +1,10 @@
 const knex = require('../../connection')
+const BaseService = require('../BaseService')
 
-class PersonPhoneService {
+class PersonPhoneService extends BaseService {
   constructor() {
+    super()
     this.tableName = 'person_phones'
-    this.id = 'id'
-  }
-
-  browse() {
-    return knex(this.tableName)
-  }
-
-  read(id) {
-    return knex.
-        select().
-        from(this.tableName).
-        where(this.id, id).
-        first()
-  }
-
-  edit(personPhone) {
-    return knex(this.tableName).
-          where(this.id,personphone.id).
-          update(personPhone)
-  }
-
-  add(personPhone) {
-    return knex(this.tableName).insert(personPhone)
-  }
-
-  delete(personphone) {
-    return knex(this.tableName).
-          where(this.id, personphone.id).
-          update(personphone.deleted_at)
   }
 }
 
