cc82dc0ce7d7f6cf0a5a332fec4f7bb9d21760a4
erdivartanovich <erdivartanovich@gmail.com>
2017-02-20 21:00:32 +0700

    #35 add commentary on base service class

--- src/Services/BaseService.js
+++ src/Services/BaseService.js
@@ -3,73 +3,89 @@ const moment = require('moment')
 const DATEFORMAT = 'YYYY-MM-DD HH:mm:ss'
 
 class BaseService {
-
-
+    /**
+     * Constructor
+     */
     constructor() {
-        this.id = 'id'
+        //set tablename to null, when extends need to intialize it with string value
+        //softDelete flag is set to default = true 
+        this.tableName = null
         this.softDelete = true
     }
 
     browse() {
+        //delete from current table where deleted at = null
         return knex(this.tableName)
+            .where('deleted_at', null)
     }
 
     read(id) {
+        //select from current table where table.id=id 
         return knex(this.tableName)
-            .where(this.id, id)
+            .where('deleted_at', null)
+            .where('id', id)
             .first()
     }
 
     edit(payload) {
-        payload = this.editTimestamp(payload)
-
+        //add timestamp before edit to payload
+        this.beforeEdit(payload)
+        
+        //update where table.id = payload.id
         return knex(this.tableName)
-            .where(this.id, payload[this.id])
+            .where('id', payload['id'])
             .update(payload)
     }
 
     add(payload) {
-        payload = this.addTimestamp(payload)
+        //add timestamp before edit to payload
+        this.beforeAdd(payload)
 
+        //insert payload to current table
         return knex(this.tableName)
             .insert(payload)
     }
 
-    delete(id, isForced) {
-        const query =  knex(this.tableName).where(this.id, id)
-
+    delete(payload, isForced) {
+        //create query delete
+        const query =  knex(this.tableName).where('id', payload['id'])
+        
+        //if isForced and isSoftDelete then add deleted timestamp
         if ( ! isForced && this.softDelete) {
             return query.update({'deleted_at': this.getNow()})
         }
-
+        //execute query
         return query.del()
     }
 
-    forceDelete(id) {
-        return this.delete(id, true)
+    forceDelete(payload) {
+        return this.delete(payload, true)
     }
 
-    addTimestamp(payload) {
+    beforeAdd(payload) {
+        //add timestamp on before add
         if (! payload.created_at) {
-            payload.created_at = (new moment).format(DATEFORMAT)
+            payload.created_at = this.getNow()
         }
 
-        payload = this.editTimestamp(payload)
-
-        return payload
+        if (! payload.updated_at) {
+            payload.updated_at = this.getNow()
+        }
     }
 
-    editTimestamp(payload) {
+    beforeEdit(payload) {
+        //add timestamp on before edit
         if (! payload.updated_at) {
-            payload.updated_at = (new moment).format(DATEFORMAT)
+            payload.updated_at = this.getNow()
         }
 
-        return payload
     }
     
     getNow() {
+        //get current timestamp
         return (new moment).format(DATEFORMAT)
     }
+
 }
 
 module.exports = BaseService
