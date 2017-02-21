7d2e4b44bede6907f8978048f24d2cbefa042114
isakh <rafiisakh@gmail.com>
2017-02-21 18:39:37 +0700

    #52 - Modifying test file

--- __tests__/Services/Workflow/ActionTriggerServiceTest.js
+++ __tests__/Services/Workflow/ActionTriggerServiceTest.js
@@ -55,19 +55,18 @@ describe('ActionTriggerService', () => {
 
     describe('#edit()' , () => {
         it('should return a valid query', (done) => {
-            const editQuery = 'update `action_triggers` set `contact_type_id` = ?, `id` = ?, `person_id` = ?, `updated_at` = ? where `id` = ?'
+            const editQuery = 'update `action_triggers` set `action_id` = ?, `id` = ?, `updated_at` = ? where `id` = ?'
             const editObject = {
-                id: 1, 'person_id': 1, 'contact_type_id': 1
+                id: 1, 'action_id': 77
             }
 
             testObj.edit(editObject).then(result => {
                 result.sql.should.equals(editQuery)
                 result.method.should.equals('update')
-                result.bindings[0].should.equals(editObject.contact_type_id)
+                result.bindings[0].should.equals(editObject.action_id)
                 result.bindings[1].should.equals(editObject.id)
-                result.bindings[2].should.equals(editObject.person_id)
-                result.bindings[3].should.equals(testObj.getNow())
-                result.bindings[4].should.equals(editObject.id)
+                result.bindings[2].should.equals(testObj.getNow())
+                result.bindings[3].should.equals(editObject.id)
 
                 done()
             }).catch(err => done(err))
@@ -76,14 +75,14 @@ describe('ActionTriggerService', () => {
 
     describe('#add()' , () => {
         it('should return a valid query', (done) => {
-            const addQuery = 'insert into `action_triggers` (`created_at`, `person_id`, `updated_at`) values (?, ?, ?)'
-            const addPersonId = 12
+            const addQuery = 'insert into `action_triggers` (`action_id`, `created_at`, `updated_at`) values (?, ?, ?)'
+            const addPersonId = 10
 
-            testObj.add({'person_id': addPersonId}).then(result => {
+            testObj.add({'action_id': addPersonId}).then(result => {
                 result.sql.should.equals(addQuery)
                 result.method.should.equals('insert')
-                result.bindings[0].should.equals(testObj.getNow())
-                result.bindings[1].should.equals(addPersonId)
+                result.bindings[1].should.equals(testObj.getNow())
+                result.bindings[0].should.equals(addPersonId)
                 result.bindings[2].should.equals(testObj.getNow())
 
                 done()
