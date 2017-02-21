7213c4cd6de3ea0679c3b11d1066e690709e97d6
isakh <rafiisakh@gmail.com>
2017-02-21 11:43:09 +0700

    #39 - Modifying sync method

--- src/Services/Person/PersonMotivationService.js
+++ src/Services/Person/PersonMotivationService.js
@@ -8,21 +8,28 @@ class PersonMotivationService extends BaseService{
 		this.tableName = 'person_motivations'
 	}
 
+	// sync(person, motivations) {
+	// 	return knex(this.tablename)
+	// 		.where('person_motivations.person_id', person.id)
+	// 		.delete()
+	// 		.then(() => {
+	// 			// loop motivations table
+	// 			motivations.map(function(motivation) {
+	// 				knex(this.tablename)
+	// 				.insert(motivation)
+	// 			})
+	// 		})
+	// }
+
 	sync(person, motivations) {
-		return knex(this.tablename)
-			.where('person_motivations.person_id', person.id)
-			.delete()
-			.then(() => {
-				// loop motivations table
-				// foreach (motivations as motivation) {
-				// 	knex(this.tablename)
-				// 	.insert(motivation)	
-				// }
-				motivations.map(function(motivation) {
-					knex(this.tablename)
-					.insert(motivation)
-				})
+		return knex.transaction(trx => {
+			return trx.from(this.tableName).where('person_id', person.id).delete().then(() => {
+				return trx.insert(motivations.map(item => {
+					item.person_id = person.id
+					return item
+				})).into(this.tableName)
 			})
+		})
 	}
 
 }
