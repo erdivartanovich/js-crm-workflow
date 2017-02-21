1da0df40adda6417809690f1e6de82bf8d0be9ca
Harry Tri Wibowo <mail@htwibowo.co>
2017-02-21 17:03:55 +0700

    #29 add unit test

--- __tests__/Services/Person/PersonContactTypeServiceTest.js
+++ __tests__/Services/Person/PersonContactTypeServiceTest.js
@@ -3,40 +3,15 @@ const PersonContactTypeService = require(basePath + '/Services/Person/PersonCont
 var tracker = require('mock-knex').getTracker()
 
 const testObj = new PersonContactTypeService()
-
-const expectedResults = [
-    {
-        id : 3,
-        person_id : 10,
-        deleted_at: null,
-    },
-    {
-        id : 2,
-        deleted_at: null,
-        person_id : 10,
-    },
-    {
-        id : 1,
-        deleted_at: null,
-        person_id : 15,
-    },
-]
+const tableName = 'person_contact_types'
 
 describe('PersonContactTypeService', () => {
 
     before(() => {
         tracker.install()
 
-        tracker.on('query', function checkResult(query, step) {
-          console.log('current step', step);
-          switch(step){
-            case 1:
-              query.response(expectedResults)
-              break
-            case 2:
-              query.response(expectedResults[2])
-              break
-          }
+        tracker.on('query', function checkResult(query) {
+            query.response(query)
         })
     })
 
@@ -44,15 +19,108 @@ describe('PersonContactTypeService', () => {
         tracker.uninstall()
     })
 
-    describe('#browse()', () => {
-        it('should return valid results', () => {
-            return testObj.browse().should.eventually.equals(expectedResults)
+    describe('#browse()' , () => {
+        it('should return a valid query', (done) => {
+            const browseQuery = 'select * from `' + tableName + '` where `deleted_at` is null'
+
+            testObj.browse(2).then(result => {
+                result.sql.should.equals(browseQuery)
+                result.method.should.equals('select')
+                done()
+            }).catch(err => done(err))
         })
     })
 
-    describe('#read()', () => {
-        it('should return a valid result', () => {
-            return testObj.read(1).should.eventually.equals(expectedResults[2])
+    describe('#read()' , () => {
+        it('should return a valid query', (done) => {
+            const readQuery = 'select * from `' + tableName + '` where `deleted_at` is null and `id` = ? limit ?'
+            const readId = 2
+            const limit = 1
+
+            testObj.read(readId).then(result => {
+                result.sql.should.equals(readQuery)
+                result.method.should.equals('first')
+                result.bindings[0].should.equals(readId)
+                result.bindings[1].should.equals(limit)
+                done()
+            }).catch(err => done(err))
+        })
+    })
+
+    describe('#edit()' , () => {
+        it('should return a valid query', (done) => {
+            const editQuery = 'update `' + tableName + '` set `contact_type_id` = ?, `id` = ?, `person_id` = ?, `updated_at` = ? where `id` = ?'
+            const editObject = {
+                id: 1, 'person_id': 1, 'contact_type_id': 1
+            }
+
+            testObj.edit(editObject).then(result => {
+                result.sql.should.equals(editQuery)
+                result.method.should.equals('update')
+                result.bindings[0].should.equals(editObject.contact_type_id)
+                result.bindings[1].should.equals(editObject.id)
+                result.bindings[2].should.equals(editObject.person_id)
+                result.bindings[3].should.equals(testObj.getNow())
+                result.bindings[4].should.equals(editObject.id)
+                done()
+            }).catch(err => done(err))
+        })
+    })
+
+    describe('#add()' , () => {
+        it('should return a valid query', (done) => {
+            const addQuery = 'insert into `' + tableName + '` (`created_at`, `person_id`, `updated_at`) values (?, ?, ?)'
+            const addPersonId = 12
+
+            testObj.add({'person_id': addPersonId}).then(result => {
+                result.sql.should.equals(addQuery)
+                result.method.should.equals('insert')
+                result.bindings[0].should.equals(testObj.getNow())
+                result.bindings[1].should.equals(addPersonId)
+                result.bindings[2].should.equals(testObj.getNow())
+                done()
+            }).catch(err => done(err))
+        })
+    })
+
+    describe('#delete()' , () => {
+        it('should return a valid query for soft delete', (done) => {
+            const deleteQuery = 'update `' + tableName + '` set `deleted_at` = ? where `id` = ?'
+            const deleteId = 12
+
+            testObj.delete({'id': deleteId}).then(result => {
+                result.sql.should.equals(deleteQuery)
+                result.method.should.equals('update')
+                result.bindings[0].should.equals(testObj.getNow())
+                result.bindings[1].should.equals(deleteId)
+                done()
+            }).catch(err => done(err))
+        })
+
+        it('should return a valid query for forced delete', (done) => {
+            const deleteQuery = 'delete from `' + tableName + '` where `id` = ?'
+            const deleteId = 12
+
+            testObj.delete({'id': deleteId}, true).then(result => {
+                result.sql.should.equals(deleteQuery)
+                result.method.should.equals('del')
+                result.bindings[0].should.equals(deleteId)
+                done()
+            }).catch(err => done(err))
+        })
+
+        it('should return a valid query for hard delete', (done) => {
+            const deleteQuery = 'delete from `' + tableName + '` where `id` = ?'
+            const deleteId = 12
+
+            testObj.softDelete = false
+
+            testObj.delete({'id': deleteId}).then(result => {
+                result.sql.should.equals(deleteQuery)
+                result.method.should.equals('del')
+                result.bindings[0].should.equals(deleteId)
+                done()
+            }).catch(err => done(err))
         })
     })
 })
