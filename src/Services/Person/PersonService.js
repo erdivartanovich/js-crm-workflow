a18aea8097fb8926b520cd2f70831dda1e5f3b51
erdivartanovich <erdivartanovich@gmail.com>
2017-02-21 14:21:35 +0700

    #30 add anotation for future rewrite

--- src/Services/Person/PersonService.js
+++ src/Services/Person/PersonService.js
@@ -9,6 +9,50 @@ class PersonService extends BaseService {
         super()
         this.tableName = 'persons'
     }
+
+    add(person) {
+        /**
+         * ***** WAIT FOR THIS SERVICES AVAILABLE *****
+         * Inject User Service here
+         *  -- Find or insert related user of person
+         */
+
+        /**
+         * Inject Stage Service
+         * -- Find or insert related stage of the person
+         */
+
+        /**
+         * Inject LeadType service
+         * -- Find or insert related LeadType of the person 
+         */
+
+        //Set user_id, stage_id and lead_type_id
+        //add person
+        super.add(person)
+    }
+
+    update(person) {
+        /**
+         * ***** WAIT FOR THIS SERVICES AVAILABLE *****
+         * Inject User Service here
+         *  -- Find or insert related user of person
+         */
+
+        /**
+         * Inject Stage Service
+         * -- Find or insert related stage of the person
+         */
+
+        /**
+         * Inject LeadType service
+         * -- Find or insert related LeadType of the person 
+         */
+
+        //Set user_id, stage_id and lead_type_id
+        //add person
+        super.apdate(person)
+    }
 }
 
 module.exports = PersonService
