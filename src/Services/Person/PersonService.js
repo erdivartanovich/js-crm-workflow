fc1cf252bf9f93a613683915a15b0c3a6e854105
erdivartanovich <erdivartanovich@gmail.com>
2017-02-22 22:59:26 +0700

    #30 add LeadType and Stage ensurement function

--- src/Services/Person/PersonService.js
+++ src/Services/Person/PersonService.js
@@ -7,6 +7,12 @@ const BaseService = require('../BaseService')
 //require UserService dependency
 const UserService = require('../User/UserService')
 
+//require StageService dependency
+const StageService = require('../Stage/StageService')
+
+//require LeadTypeService dependency
+const LeadTypeService = require('../Person/LeadTypeService')
+
 class PersonService extends BaseService {
     /**
      * Constructor
@@ -14,7 +20,15 @@ class PersonService extends BaseService {
     constructor() {
         super()
         this.tableName = 'persons'
+
+        //Inject User Service here
         this.user = new UserService
+
+        //inject StageService here
+        this.stage = new StageService
+
+        //inject LeadTypeService here
+        this.lead_type = new LeadTypeService
     }
 
     getUser(user_id) {
@@ -30,13 +44,54 @@ class PersonService extends BaseService {
             }
         })
     }
+
+    findOrAddStage(stage) {
+        return new Promise((resolve, reject) => {
+            if (typeof stage === 'undefined' || typeof stage.id === 'undefined') {
+                reject(stage)
+            } else {
+                this.stage.read(stage.id)
+                .then((result) => {
+                    if (typeof result != 'undefined') {
+                        resolve(result.id)
+                    } else {
+                        reject(stage)
+                    }
+                })
+            }
+        })
+        .catch ((stage) => {
+            this.stage.add(stage).returning('id')
+            .then((result) => result)
+        })
+    }
+    
+    findOrAddLeadType(lead_type) {
+        return new Promise((resolve, reject) => {
+            if (typeof lead_type === 'undefined' || typeof lead_type.id === 'undefined') {
+                reject(lead_type)
+            } else {
+                this.lead_type.read(lead_type.id)
+                .then((result) => {
+                    if (typeof result != 'undefined') {
+                        resolve(result.id)
+                    } else {
+                        reject(lead_type)
+                    }
+                })
+            }
+        })
+        .catch ((lead_type) => {
+            this.lead_type.add(lead_type).returning('id')
+            .then((result) => result)
+        })
+    }
     
     add(person) {
+       
         /**
-         * Inject User Service here
-         *  -- Find user enitity of the person
-         */
-
+         * then Find user enitity of the person
+         */ 
         //get user 
         this.getUser(person.user_id) 
             // got the user but maybe undefined
@@ -44,22 +99,14 @@ class PersonService extends BaseService {
                 //ensure user is valid
                 this.ensureUser(user)) 
             
-        /**
-         * Inject Stage Service
-         * -- Find or insert related stage of the person
-         */
-        
+        // Find or insert related stage of the person
+        .then(() => this.findOrAddStage(person.stage))
 
-        /**
-         * Inject LeadType service
-         * -- Find or insert related LeadType of the person 
-         */
+        // Find or insert related LeadType of the person 
+        .then(() => this.findOrAddLeadType)
 
-        .then((user_id) => console.log('user id is: ', user_id))
-            .catch((message) => console.log('error ! : ', message))
-        //Set user_id, stage_id and lead_type_id
         //add person
-        super.add(person)
+        //super.add(person)
     }
 
     update(person) {
