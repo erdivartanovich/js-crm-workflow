ad733daa52877f28be50310b770d405ab1431819
Syamsul Anwar <msyamsulanwr@gmail.com>
2017-02-22 13:22:00 +0700

    #58 Rewrite Custom Field Service and add unit testing

---
+++ src/Services/CustomField/CustomFieldService.js
@@ -0,0 +1,16 @@
+const knex = require('../../connection')
+const BaseService = require('../BaseService')
+
+class CustomFieldService extends BaseService{
+    /**
+     * constructor
+     */
+    constructor() {
+        super()
+        // set table name
+        this.tableName = 'custom_fields'
+    }
+
+}
+
+module.exports = CustomFieldService
