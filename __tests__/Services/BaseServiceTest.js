058acce438b59c7fe9f4e5ac7bc7c31b05c6f942
erdivartanovich <erdivartanovich@gmail.com>
2017-02-22 14:03:28 +0700

    #38 write test for read, edit and add

--- __tests__/Services/BaseServiceTest.js
+++ __tests__/Services/BaseServiceTest.js
@@ -5,6 +5,11 @@ const BaseService = require(basePath + '/Services/BaseService')
 var tracker = require('mock-knex').getTracker()
 
 const testClass = new BaseService()
+/**
+ * should test the base-service against specifif table name, 
+ * because base-service's table name is null by default
+ */
+testClass.tableName = 'person_family'
 const tableName = testClass.tableName
 
 describe('====== BaseServiceTest ========', () => {
@@ -23,9 +28,9 @@ describe('====== BaseServiceTest ========', () => {
 
     describe('#browse()' , () => {
         it('should return a valid query', (done) => {
-            const browseQuery = 'select * from `' + tableName + '` where `deleted_at` is null'
+            const query = 'select * from `' + tableName + '` where `deleted_at` is null'
             testClass.browse().then(result => {
-                result.sql.should.equals(browseQuery)
+                result.sql.should.equals(query)
                 result.method.should.equals('select')
                 done()
             }).catch(err => done(err))    
@@ -34,19 +39,53 @@ describe('====== BaseServiceTest ========', () => {
 
     describe('#read()' , () => {
         it('should return a valid query', (done) => {
-            
+            const query = 'select * from `' + tableName + '` where `deleted_at` is null and `id` = ? limit ?'
+            const id = 2
+            const limit = 1
+
+            testClass.read(id).then(result => {
+                result.sql.should.equals(query)
+                result.method.should.equals('first')
+                result.bindings[0].should.equals(id)
+                result.bindings[1].should.equals(limit)
+                done()
+            }).catch(err => done(err))
         })
     })
 
     describe('#edit()' , () => {
         it('should return a valid query', (done) => {
-            c
+            const query = 'update `'+ tableName +'` set `id` = ?, `label` = ?, `person_id` = ?, `related_to` = ?, `relative_type` = ?, `updated_at` = ? where `id` = ?' 
+            const mockObject = {id: 4, person_id: 3, related_to: 53, relative_type: 3, label: 'illum'}
+            testClass.edit(mockObject).then(result => {
+                result.sql.should.equals(query)
+                result.method.should.equals('update')
+                result.bindings[0].should.equals(mockObject.id)
+                result.bindings[1].should.equals(mockObject.label)
+                result.bindings[2].should.equals(mockObject.person_id)
+                result.bindings[3].should.equals(mockObject.related_to)
+                result.bindings[4].should.equals(mockObject.relative_type)
+                done()
+            }).catch(err => done(err))
         })
     })
 
     describe('#add()' , () => {
         it('should return a valid query', (done) => {
+            const query = 'insert into `' + tableName + '` (`created_at`, `id`, `label`, `person_id`, `related_to`, `relative_type`, `updated_at`) values (?, ?, ?, ?, ?, ?, ?)'
+            const mockObject = {id: 4, person_id: 3, related_to: 53, relative_type: 3, label: 'illum'}
             
+            testClass.add(mockObject).then(result => {
+                result.sql.should.equals(query)
+                result.method.should.equals('insert')
+                result.bindings[1].should.equals(mockObject.id)                
+                result.bindings[2].should.equals(mockObject.label)
+                result.bindings[3].should.equals(mockObject.person_id)
+                result.bindings[4].should.equals(mockObject.related_to)
+                result.bindings[5].should.equals(mockObject.relative_type)
+                                                 
+                done()
+            }).catch(err => done(err))
         })
     })
 
