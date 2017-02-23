dae3edd7fad5e169bc3210c6d4ec937cddcebae2
kris <kristoforusrp@gmail.com>
2017-02-23 17:34:42 +0700

    #33 Initial person crawl service

---
+++ src/Services/Person/PersonCrawlService.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const BaseService = require('../BaseService')
+
+class PersonCrawl extends BaseService {
+    constructor() {
+        super()
+        this.person_social_accounts = 'person_social_accounts'
+    }
+}
