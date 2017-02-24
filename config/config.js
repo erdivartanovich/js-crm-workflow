231cfb160cf70cfbd9bed732b54202c41eb298e9
Harry Tri Wibowo <mail@htwibowo.co>
2017-02-24 16:12:44 +0700

    #71 Revert config.js

--- config/config.js
+++ config/config.js
@@ -5,11 +5,11 @@ const envloader = require('./Envloader')
 const config = {
     storage : {
         db1: {
-            host: envloader.get('DB_HOST').whenEmpty('127.0.0.1'),
+            host: envloader.get('DB_HOST').whenEmpty('localhost'),
             port: envloader.get('DB_PORT').whenEmpty(3306),
-            database: envloader.get('DB_DATABASE').whenEmpty('crm'),
-            user: envloader.get('DB_USERNAME').whenEmpty('root'),
-            password: envloader.get('DB_PASSWORD').whenEmpty('root')
+            database: envloader.get('DB_DATABASE').whenEmpty('homestead'),
+            user: envloader.get('DB_USERNAME').whenEmpty('homestead'),
+            password: envloader.get('DB_PASSWORD').whenEmpty('secret')
         },
         db2: {
             host: envloader.get('DB_HOST').whenEmpty('localhost'),
