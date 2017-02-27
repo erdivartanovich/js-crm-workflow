877ac3dc28a30eda8f54fd169b57a7873b534192
awebr000 <mas.awe.awe@gmail.com>
2017-02-27 18:09:06 +0700

    fix Sms.js file

--- src/Services/Action/Sms.js
+++ src/Services/Action/Sms.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const interactionService = require('../Interaction/InteractionService')
-const moment = require('../../moment')
+const moment = require('moment')
 const phoneService = require('../Person/PersonPhoneService')
 // FIXME with add interactionInterface
 
@@ -10,65 +10,69 @@ class Sms {
     constructor() {
         //FIXME with KWApi js
         this.api = 'KWApi'
-        this.interaction = interactionInterface;
-        this.interactionService = interactionService;
-        this.phoneService = phoneService;
+        this.interactionService = interactionService
+        this.phoneService = phoneService
 
     }
 
     send(workflow, action, person, message) {
 
         //FIXME
-        const communicationTemplate = action.getTemplate();
-        if (template) {
-            const message = communicationTemplate.setTemplateData(person, workflow.getOwner());
+        const communicationTemplate = action.getTemplate()
+        if (communicationTemplate) {
+            const message = communicationTemplate.setTemplateData(person, workflow.user_id)
         }
         this.phoneService.browse().where({person_id: person.id}).then(data => {
-        const phones = data;
+            const phones = data
 
-        phones.forEach(function(phone) {
-            const res = this.api.communication().sendText(phone.getNumber(), message);
+            phones.forEach(function(phone) {
+                const res = this.api.communication().sendText(phone.number, message)
 
-            if (!res.isError()) {
-                const INITIATED_BY_USER = 1;
-                const INTERACTION_TYPE_TEXT = 4;
-
-                this.interaction.setPerson(person);
-                this.interaction.setUser(workflow.getOwner());
-                this.interaction.setInteractionType(INTERACTION_TYPE_TEXT);
-                this.interaction.setInteractionAt(moment().format('Y-m-d H:i:s'));
-                this.interaction.setPhoneNumber(phone.getNumber());
-                this.interaction.setInitiatedBy(INITIATED_BY_USER);
-                this.interactionService.add(this.interaction);
-            }
-        }, this);
-        });
+                if (!res.isError()) {
+                    const INITIATED_BY_USER = 1
+                    const INTERACTION_TYPE_TEXT = 4
+                    const interaction = {
+                        person_id: person.id,
+                        user_id: workflow.user_id,
+                        interaction_type: INTERACTION_TYPE_TEXT,
+                        interaction_at: moment().format('Y-m-d H:i:s'),
+                        phone_number: phone.number,
+                        initiated_by: INITIATED_BY_USER,
+                    }
+                    this.interactionService.add(interaction)
+                }
+            }, this)
+        })
     }
 
     sendPrimary(workflow, action, person, message) {
         // FIXME
-        const communicationTemplate = action.getTemplate();
+        const communicationTemplate = action.getTemplate()
         if (communicationTemplate) {
-            const message = communicationTemplate.setTemplateData(person, workflow.getOwner());
+            const message = communicationTemplate.setTemplateData(person, workflow.user_id)
         }
         this.phoneService.browse().where({is_primary: 1, person_id: person.id}).first().then(data => {
-            const phone = data;
+            const phone = data
             if (phone) {
-            const res = this.api.communication().sendText(phone.getNumber(), $message);
+                const res = this.api.communication().sendText(phone.number, message)
 
-            if (!res.isError()) {
-                    const INITIATED_BY_USER = 1;
-                    const INTERACTION_TYPE_TEXT = 4;
-
-                    this.interaction.setPerson(person);
-                    this.interaction.setUser(workflow.getOwner());
-                    this.interaction.setInteractionType(INTERACTION_TYPE_TEXT);
-                    this.interaction.setInteractionAt(moment().format('Y-m-d H:i:s'));
-                    this.interaction.setPhoneNumber(phone.getNumber());
-                    this.interaction.setInitiatedBy(INITIATED_BY_USER);
-                    this.interactionService.add(this.interaction);
+                if (!res.isError()) {
+                    const INITIATED_BY_USER = 1
+                    const INTERACTION_TYPE_TEXT = 4
+                    
+                    const interaction = {
+                        person_id: person.id,
+                        user_id: workflow.user_id,
+                        interaction_type: INTERACTION_TYPE_TEXT,
+                        interaction_at: moment().format('Y-m-d H:i:s'),
+                        phone_number: phone.number,
+                        initiated_by: INITIATED_BY_USER,
+                    }
+                    this.interactionService.add(interaction)
                 }                                
             }
-        });
+        })
     }
 }
+
+module.exports = Sms
