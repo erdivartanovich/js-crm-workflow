9973b8336329c09f30e86f42994e581976901769
Syamsul Anwar <msyamsulanwr@gmail.com>
2017-02-27 15:27:27 +0700

    #87 create getFieldValue method

--- src/Services/CustomField/ObjectCustomFieldService.js
+++ src/Services/CustomField/ObjectCustomFieldService.js
@@ -1,5 +1,5 @@
 'use strict'
-
+s
 const knex = require('../../connection')
 const BaseService = require('../BaseService')
 
@@ -13,6 +13,28 @@ class ObjectCustomFieldService extends BaseService{
         this.tableName = 'object_custom_fields'
     }
 
+    getFieldValue(user) {
+
+      return knex(this.tableName)
+      let attributes = {
+        user_id: user['user_id'],
+        custom_field_id: user['custom_field_id'],
+        object_id: user['object_id'],
+        object_type: user['object_type']
+      }
+
+      let objectField = knex(this.tableName).where(attributes).first()
+
+      if (objectField) {
+        return objectField
+      }
+
+      return knex(this.tableName)
+            .insert(attributes, user)
+
+  }
+
+    }
 }
 
 module.exports = ObjectCustomFieldService
