16f2cff9223029117d1914a164ef7188a9d1f132
hystolytc <melife4@gmail.com>
2017-02-27 23:30:24 +0700

    #94 - adding firstOrCreate() in SocialNetwork Service

--- src/Services/SocialNetwork/SocialNetworkService.js
+++ src/Services/SocialNetwork/SocialNetworkService.js
@@ -8,8 +8,43 @@ class SocialNetworkService extends BaseService{
     constructor(){
         super()
         this.tableName = 'social_networks'
+        this.model = knex(this.tableName)
     }
     
+    applyConditions(attributes){
+        for(var value in attributes){
+            if(Array.isArray(attributes[value])){
+                let field, condition, val
+                [field, condition, val] = attributes[value]
+    
+                return this.model.where(field, condition, val).first().then(result => {
+                    this.model = result
+                })
+            
+            }else{
+                
+                return this.model.where(value, attributes[value]).first().then(result => { 
+                    this.model = result 
+                })
+            
+            }
+            
+        }
+
+    }
+
+    firstOrCrete(attributes){
+        return this.applyConditions(attributes).then(result => {
+            let model = this.model
+            this.resetModel()
+            return model === undefined ? this.add(attributes) : model
+        })
+    }
+
+    resetModel(){
+        this.model = knex(this.tableName)
+    }
+
     listsDefaults(user){
         return knex(this.tableName)
                    .whereNull('user_id')
