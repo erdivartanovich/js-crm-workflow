492d2b7625d8848eafcf557274816be0c39ddf18
isakh <rafiisakh@gmail.com>
2017-02-27 21:31:15 +0700

    #96 - add sync, getBy, and setAsPrimary method

--- src/Services/Referral/ReferralService.js
+++ src/Services/Referral/ReferralService.js
@@ -10,12 +10,69 @@ class ReferralService extends BaseService {
 		this.tableName = 'referrals'
 	}
 
-	getBy(source, referrer) {
+	sync(referrerable, sources) {
+		return knex.transaction(trx => {
+			return trx.from(this.tableName)
+				.where({
+					referrerable_id: referrerable.referrerable_id,
+					referrerable_type: referrerable.referrerable_type
+				})
+				.delete()
+				.then(() => {
+					return trx.insert(sources.map(item => {
+						item.referrerable_id = referrerable.referrerable_id
+						item.referrerable_type = referrerable.referrerable_type
+						return item
+					})).into(this.tableName)
+				})
+		})
+	}
 
+	getBy(source, referrer) {
+		
+		const newObject = {
+			source_id: source.id,
+			source_type: 'sources',
+			referrerable_id: referrer.id,
+			referrerable_type: 'persons'
+		}
+
+		return knex(this.tableName)
+			.where({
+				source_id: source.id,
+				source_type: 'sources',
+				referrerable_id: referrer.id,
+				referrerable_type: 'persons'
+			}).first()
+		.then(result => {
+			if(typeof result ===  'undefined') {
+				this.add(newObject)
+					.then(() => {
+						return knex(this.tableName).orderBy('id', 'desc').first()
+					})
+					.then(result => {
+						return result
+					})
+			}
+			// console.log(result)
+			return result
+		})
 	}
 
 	setAsPrimary(referral) {
-		
+		//reset current is_primary
+		return knex(this.tableName)
+			.where({
+				referrerable_id: referral.referrerable_id,
+				referrerable_type: referral.referrerable_type,
+				is_primary: 1
+			})
+			.update('is_primary', 0)
+		.then(() => {
+			return knex(this.tableName)
+				.where('id', referral.id)
+				.update('is_primary', 1)
+		})
 	}
 }
 
