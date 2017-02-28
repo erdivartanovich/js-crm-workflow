77ae628bdff8b7780b517064d6dd99ef09d233d7
hystolytc <melife4@gmail.com>
2017-02-28 12:12:22 +0700

    #86 - adding exception in add() and edit() for checking social_network_id if it exist before add or editing data

--- src/Services/Person/PersonSocialAccountService.js
+++ src/Services/Person/PersonSocialAccountService.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const knex = require('../../connection')
 const BaseService = require('../BaseService')
 
 class PersonSocialAccountService extends BaseService{
@@ -7,8 +8,32 @@ class PersonSocialAccountService extends BaseService{
     constructor(){
         super()
         this.tableName = 'person_social_accounts'
+        this.socialNetworkServiceTable = knex('social_networks')
+    }
+
+    add(socialAccount){
+        return this.socialNetworkServiceTable.where('id', socialAccount.social_network_id).first()
+                .then(result => {
+                    if(result === undefined){
+                        throw 'Person Social Account need valid social network id.'
+                    }
+
+                    return knex(this.tableName).insert(socialAccount)
+                })
+    }
+
+    edit(socialAccount){
+        return this.socialNetworkServiceTable.where('id', socialAccount.social_network_id).first()
+                .then(result => {
+                    if(result === undefined){
+                        throw 'Person Social Account need valid social network id.'
+                    }
+
+                    return knex(this.tableName).where('id', socialAccount['id']).update(socialAccount)
+                })
     }
 
 }
+
 
 module.exports = PersonSocialAccountService
