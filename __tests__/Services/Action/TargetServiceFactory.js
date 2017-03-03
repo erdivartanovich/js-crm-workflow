e0cd380df554587919233d959debac0de0727d71
Harry Tri Wibowo <mail@htwibowo.co>
2017-03-03 15:06:31 +0700

    refactory-id/kw-crm#83 Add TargetServiceFactory test

---
+++ __tests__/Services/Action/TargetServiceFactory.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const sourcePath = '../../../src'
+const TargetServiceFactory = require(sourcePath + '/Services/Action/TargetServiceFactory')
+const PersonService = require(sourcePath + '/Services/Person/PersonService')
+
+let testObject = null
+
+describe('TargetServiceFactory' , () => {
+    describe('#make()', () => {
+        it('should return a valid service instance', () => {
+            testObject = new TargetServiceFactory({target_class: 'persons'})
+            testObject.make().should.be.instanceOf(PersonService)
+        })
+    })
+})
