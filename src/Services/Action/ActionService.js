559dedaba04c99d66217a03f115e5a7711c61670
Harry Tri Wibowo <mail@htwibowo.co>
2017-03-06 11:39:55 +0700

    #85 clean up

--- src/Services/Action/ActionService.js
+++ src/Services/Action/ActionService.js
@@ -6,47 +6,47 @@ const knex = require('../../connection')
 
 class ActionService extends BaseService {
 
-	constructor() {
-		super()
-		this.tableName = 'actions'
-	}
+    constructor() {
+        super()
+        this.tableName = 'actions'
+    }
 
-	syncWorkFlows(action, workflows) {
+    syncWorkFlows(action, workflows) {
 		//FIXME
-		const ids = workflows.map((workflow) => {
-			return workflow.workflow_id
-		})
+        const ids = workflows.map((workflow) => {
+            return workflow.workflow_id
+        })
 
-		return knex.transaction(trx => {
-			return trx.from('action_workflow').where('action_id', action.id).delete().then(() => {
-				return trx.insert(ids.map(item => {
-					item.action_id = action.id
-					return item
-				})).into(this.tableName)
-			})
-		})
-	}
+        return knex.transaction(trx => {
+            return trx.from('action_workflow').where('action_id', action.id).delete().then(() => {
+                return trx.insert(ids.map(item => {
+                    item.action_id = action.id
+                    return item
+                })).into(this.tableName)
+            })
+        })
+    }
 
-	syncRules(action, rules) {
+    syncRules(action, rules) {
 		//TODO
-	}
+    }
 
-	fireAction(isRunnableOnce, workflow, action, objects, rules) {
-		this.executor = new ActionExecutor(workflow, action, objects, rules)
+    fireAction(isRunnableOnce, workflow, action, objects, rules) {
+        this.executor = new ActionExecutor(workflow, action, objects, rules)
 
-		return isRunnableOnce? this.executor.runnableOnce().execute() : this.executor.execute()
-	}
+        return isRunnableOnce? this.executor.runnableOnce().execute() : this.executor.execute()
+    }
 
-	getExecutor() {
-		 return this.executor
-	}
+    getExecutor() {
+        return this.executor
+    }
 
-	getActionWorkflow(workflow, action) {
+    getActionWorkflow(workflow, action) {
 		//FIXME
-		return knex('actions')
+        return knex('actions')
 			.join('action_workflow', 'action.id', 'workflow_id')
 			.select('id')
-	}
+    }
 }
 
 module.exports = ActionService
