2a5d8612b4156fb2cb57490b0b085cb0bae2eaf0
isakh <rafiisakh@gmail.com>
2017-03-07 16:04:38 +0700

    #115 - Renaming sms service namespace

--- src/Infrastructures/Target/Sms.js
+++ src/Infrastructures/Target/Sms.js
@@ -1,39 +1,43 @@
 'use strict'
 
-const interactionService = require('../Interaction/InteractionService')
+const interactionService = require('../../Services/Interaction/InteractionService')
 const moment = require('moment')
-const phoneService = require('../Person/PersonPhoneService')
-const communicationTemplateService = require('../CommunicationTemplate/CommunicationTemplateService')
-// FIXME with add interactionInterface
+const phoneService = require('../../Services/Person/PersonPhoneService')
+const communicationTemplateService = require('../../Services/CommunicationTemplate/CommunicationTemplateService')
+const KWApi = require('../../../../kwapi-wrapper-js/lib/KWApi/index')
+
+const INTERACTION_TYPE_TEXT = 4
+const INITIATED_BY_USER = 1
 
 class Sms extends communicationTemplateService{
 
     constructor() {
         //FIXME with KWApi js
         super()
-        this.api = 'KWApi'
+        // this.api = 'KWApi'
+        this.api = KWApi
         this.interactionService = new interactionService()
         this.phoneService = new phoneService()
         this.communicationTemplateService = new communicationTemplateService()
     }
 
     send(workflow, action, person, message) {
+        const phones = []
         this.communicationTemplateService.read(action.template_id).then(response => {
-            const communicationTemplate = response 
+            const communicationTemplate = response
             if (communicationTemplate) {
                 message = communicationTemplate.template
             }
-            this.phoneService.readBy('person_id', person.id).then(data => {
+            this.phoneService.getNumbers('person_id', person.id).then(data => {
 
                 const phones = data
-
+                console.log(phones)
                 phones.forEach(function(phone) {
                     //FIXME with KWApi.
                     const res = this.api.communication().sendText(phone.number, message)
 
                     if (!res.isError) {
-                        const INITIATED_BY_USER = 1
-                        const INTERACTION_TYPE_TEXT = 4
+
                         const interaction = {
                             person_id: person.id,
                             user_id: workflow.user_id,
@@ -53,19 +57,20 @@ class Sms extends communicationTemplateService{
         // FIXME with add KWApi
 
         this.communicationTemplateService.read(action.template_id).then(response => {
-            const communicationTemplate = response 
+            const communicationTemplate = response
             if (communicationTemplate) {
                 message = communicationTemplate.template
             }
-            this.phoneService.getPrimary('person_id', person.id).first().then(data => {
+            this.phoneService.getPrimary('person_id', person.id)
+            .then(data => {
                 const phone = data
+                console.log(phone)
                 if (phone) {
-                    const res = this.api.communication().sendText(phone.number, message)
+                    console.log(this.api)
+                    const res = this.api['Communication'].sendText(phone.number, message)
 
                     if (!res.isError) {
-                        const INITIATED_BY_USER = 1
-                        const INTERACTION_TYPE_TEXT = 4
-                        
+
                         const interaction = {
                             person_id: person.id,
                             user_id: workflow.user_id,
@@ -75,7 +80,7 @@ class Sms extends communicationTemplateService{
                             initiated_by: INITIATED_BY_USER,
                         }
                         this.interactionService.add(interaction)
-                    }    
+                    }
                 }
             })
         })
