5830ce39d8922aaf48b854308d78a3facde31301
Harry Tri Wibowo <mail@htwibowo.co>
2017-03-09 00:22:33 +0700

    refactory-id/kw-crm#117 add crawlPerson and crawlPersonById

--- src/Services/Person/PersonCrawlService.js
+++ src/Services/Person/PersonCrawlService.js
@@ -9,7 +9,7 @@ const PersonService = require('./PersonService')
 const Moment = require('moment')
 
 const parse = require('libphonenumber-js').parse
-const asYouType =require('libphonenumber-js').asYouType
+const asYouType = require('libphonenumber-js').asYouType
 
 const knex = require('../connection')
 const _ = require('lodash')
@@ -22,6 +22,62 @@ class PersonCrawlService extends BaseService {
         this.personService = new PersonService()
     }
 
+    crawlPerson(person, user) {
+        let tempEmails = []
+
+        // Get person emails by person.id
+        return this
+            .personEmail
+            .resetConditions()
+            .where('person_id', '=', person.id)
+            .browse()
+            .then(emails => {
+                tempEmails = emails
+                const promises = []
+
+                // Iterate the emails and make a Promise for every email
+                _.map(emails, email => {
+                    promises.push(this.crawlEmail(email.email, person, user))
+                })
+
+                return Promise.all(promises)
+            })
+            .then(results => {
+                const retVal = []
+
+                // Reiterate emails so retVal can be populated.
+                // This can be done since return value of Promise.all is ordered
+                let idx = 0
+                _.map(tempEmails, email => {
+                    retVal[email.email] = results[idx]
+
+                    idx += 1
+                })
+
+                return retVal
+            })
+    }
+
+    /**
+     * @todo replace this method with proper one
+     */
+    crawlEmail(email, person, user) {
+        return Promise.resolve(true)
+    }
+
+    /**
+     * Crawl social media data of a Person by personId.
+     *
+     * @param integer   id of a person
+     * @return mixed
+     */
+    crawlPersonById(personId) {
+        return this
+            .personService
+            .read(personId)
+            .then(person => this.crawlPerson(person))
+    }
+
     /**
      * Set social account object and save todatabase
      * @param person
@@ -31,56 +87,53 @@ class PersonCrawlService extends BaseService {
      */
 
     setSocialAccount(person, social, socialNetworkId) {
-        
+
         //set the db model
-        const socialAccountModel =  knex('person_social_accounts')
+        const socialAccountModel = knex('person_social_accounts')
 
         //  find current social network account on person
-        return socialAccountModel.where({
-            person_id: person.id,
-            social_network_id: socialNetworkId,
-        })
-        .first()
-        .then(socialAccount => {
+        return socialAccountModel
+            .where({person_id: person.id, social_network_id: socialNetworkId})
+            .first()
+            .then(socialAccount => {
 
-            //if not found init new empty object
-            if (typeof socialAccount == 'undefined') {
-                socialAccount = {}
-            }
+                //if not found init new empty object
+                if (typeof socialAccount == 'undefined') {
+                    socialAccount = {}
+                }
 
-            //set identifier with value of url or username or id
-            if (typeof social['url'] != 'undefined') {
-                socialAccount['identifier'] = social['url']
-            }
+                //set identifier with value of url or username or id
+                if (typeof social['url'] != 'undefined') {
+                    socialAccount['identifier'] = social['url']
+                }
 
-            if (typeof social['username'] != 'undefined') {
-                socialAccount['identifier'] = social['username']
-            }
+                if (typeof social['username'] != 'undefined') {
+                    socialAccount['identifier'] = social['username']
+                }
 
-            if (typeof social['id'] != 'undefined') {
-                socialAccount['identifier'] = social['id']
-            }
+                if (typeof social['id'] != 'undefined') {
+                    socialAccount['identifier'] = social['id']
+                }
 
-            //set social network id
-            socialAccount['social_network_id'] = socialNetworkId
+                //set social network id
+                socialAccount['social_network_id'] = socialNetworkId
 
-            //set last_crawled_date
-            socialAccount['last_crawled_date'] = this.getNow()
+                //set last_crawled_date
+                socialAccount['last_crawled_date'] = this.getNow()
 
-            //save social object into data in json string
-            socialAccount['data'] = JSON.stringify(social)
-            
-            //if id not undefined update
-            if (typeof socialAccount.id != 'undefined') {
-                return socialAccountModel.update(socialAccount)
-            }
-            
-            //if undefined insert new
-            socialAccount['person_id'] = person.id
-            return socialAccountModel.insert(socialAccount)
+                //save social object into data in json string
+                socialAccount['data'] = JSON.stringify(social)
 
-        })
+                //if id not undefined update
+                if (typeof socialAccount.id != 'undefined') {
+                    return socialAccountModel.update(socialAccount)
+                }
 
+                //if undefined insert new
+                socialAccount['person_id'] = person.id
+                return socialAccountModel.insert(socialAccount)
+
+            })
 
     }
 
@@ -92,24 +145,26 @@ class PersonCrawlService extends BaseService {
                 const phoneNumber = parse(number, 'US')
                 const countryCode = asYouType(phoneNumber).country_phone_code
 
-                phone.person_id= person.id
-                phone.label= 'Datafinder Phone'
-                phone.number= number
-                phone.country_code= countryCode
-                phone.is_current= false
-                phone.is_primary= false
+                phone.person_id = person.id
+                phone.label = 'Datafinder Phone'
+                phone.number = number
+                phone.country_code = countryCode
+                phone.is_current = false
+                phone.is_primary = false
             }
-        })
-        .then((phone) => {
-            return this.personPhoneService.browse().where({ person_id: phone.person_id, number: phone.number})
-                    .first()
-        })
-        .then(res => {
+        }).then((phone) => {
+            return this
+                .personPhoneService
+                .browse()
+                .where({person_id: phone.person_id, number: phone.number})
+                .first()
+        }).then(res => {
             if (typeof res == 'undefined') {
-                this.PersonPhoneService.create(phone)
+                this
+                    .PersonPhoneService
+                    .create(phone)
             }
-        })
-        .then((data) => data)
+        }).then((data) => data)
     }
 }
 
