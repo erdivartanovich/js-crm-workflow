ec783551f3084efe88f17fee4da648a5133d5a88
htwibowo <mail@htwibowo.co>
2017-03-10 05:27:42 +0700

    refactory-id/kw-crm#133 implementing observer

--- src/Jobs/Action/ActionResourcesJob.js
+++ src/Jobs/Action/ActionResourcesJob.js
@@ -12,11 +12,13 @@ const LOG_STATUS_SUCCESS = 1
 const LOG_STATUS_FAILED = 0
 
 class ActionResourcesJob {
-    constructor(workflow, action, resources, rules) {
+    constructor(workflow, action, resources, rules, event) {
         this.workflow = workflow
         this.action = action
         this.resources = resources
         this.rules = rules
+
+        this.event = event
     }
 
     runnableOnce(runnableOnce) {
@@ -27,8 +29,18 @@ class ActionResourcesJob {
         this.taskService = container['TaskService']
         this.logService = container['LogService']
         this.ruleService = container['RuleService']
-        // TODO:
+            // TODO:
         this.service = service
+
+        this.event.run((states) => {
+            if (!states['total']) {
+                states['total'] = 0
+            }
+
+            states['total'] += this.resources.length
+
+            this.event.emit('check')
+        })
 
         _.map(this.resources, (resource) => {
             this.processResource(resource, service)
@@ -36,47 +48,52 @@ class ActionResourcesJob {
 
     }
 
+    finished() {
+        this.event.emit('finished')
+    }
+
     processResource(resource, service) {
         return this.logService.isRunned(this.workflow, this.action, resource)
-        .then(exist => {
-            //1. check run once
-            if(exist && this.runnableOnce) {
-                return false
-            } else {
-                //process action
-                return this.applyAction(resource, service)
-            }
+            .then(exist => {
+                //1. check run once
+                if (exist && this.runnableOnce) {
+                    this.finished()
+                    return false
+                } else {
+                    //process action
+                    return this.applyAction(resource, service)
+                }
 
-            // TODO:
-            //2. check priority
-            //3. check dependency rules
-        })
+                // TODO:
+                //2. check priority
+                //3. check dependency rules
+            })
     }
 
     applyAction(resource, service) {
         let action = false
         const actionType = this.action.action_type
 
-        switch(actionType) {
-        case 1:
-            this.actionUpdate(resource, service)
-            action = true
-            break
-        case 2:
-            this.actionExecute(resource)
-            action = true
-            break
-        case 3:
-            this.actionClone(resource)
-            action = true
-            break
-        case 4:
-            this.actionAssign(resource)
-            action = true
-            break
+        switch (actionType) {
+            case 1:
+                this.actionUpdate(resource, service)
+                action = true
+                break
+            case 2:
+                this.actionExecute(resource)
+                action = true
+                break
+            case 3:
+                this.actionClone(resource)
+                action = true
+                break
+            case 4:
+                this.actionAssign(resource)
+                action = true
+                break
         }
 
-        if(!action) {
+        if (!action) {
             return this.log(resource, 0, 'No action applied')
         }
 
@@ -87,131 +104,137 @@ class ActionResourcesJob {
         // @todo: clean up the bugs
 
         return this.getActionResource(resource, resourceService)
-        .then(target => {
-            target[this.action.target_field] = this.action.value
- 
-            return this.service.edit(target).then((res) => {
+            .then(target => {
+                target[this.action.target_field] = this.action.value
+
+                return this.service.edit(target).then((res) => {})
             })
-        })
-        .then(result => {
-            if(result) {
-                return this.log(resource, 1, 'Target updated')
-                .then(() => {
-                    return true
-                })
-            } else {
-                return this.log(resource, LOG_STATUS_FAILED, 'Target updated failed')
-                .then(() => {
-                    return false
-                })
-            }
-        })
+            .then(result => {
+                if (result) {
+
+                    return this.log(resource, 1, 'Target updated')
+                        .then(() => {
+                            this.finished()
+                            return true
+                        })
+                } else {
+                    return this.log(resource, LOG_STATUS_FAILED, 'Target updated failed')
+                        .then(() => {
+                            this.finished()
+                            return false
+                        })
+                }
+            })
     }
 
     actionExecute(resource) {
-        let message = 'Action '+this.action.target_field+' on '+this.action.target_class+' not found !'
+        let message = 'Action ' + this.action.target_field + ' on ' + this.action.target_class + ' not found !'
 
-        if(typeof this.service[this.action.target_field] == 'function') {
+        if (typeof this.service[this.action.target_field] == 'function') {
             return this.getExecuteParams(resource)
-            .then(params => {
-                return this.service[this.action.target_field].apply(undefined, params)
-            })
-            .then(result => {
-                if(result) {
-                    return this.log(resource, LOG_STATUS_SUCCESS, `Action ${this.action.name} executed`)
-                    .then(() => {
-                        return Promise.resolve(true)
-                    })
-                }
-                else {
-                    return this.log(resource, LOG_STATUS_FAILED, `Action ${this.action.target_field} on ${this.action.target_class} failed!`)
-                }
+                .then(params => {
+                    return this.service[this.action.target_field].apply(undefined, params)
+                })
+                .then(result => {
+                    if (result) {
+                        return this.log(resource, LOG_STATUS_SUCCESS, `Action ${this.action.name} executed`)
+                            .then(() => {
+                                this.finished()
+                                return Promise.resolve(true)
+                            })
+                    } else {
+                        this.finished()
+                        return this.log(resource, LOG_STATUS_FAILED, `Action ${this.action.target_field} on ${this.action.target_class} failed!`)
+                    }
 
-            })
+                })
         } else {
             return this.log(resource, LOG_STATUS_FAILED, message)
-            .then(() => {
-                return Promise.resolve(false)
-            })
+                .then(() => {
+                    this.finished()
+                    return Promise.resolve(false)
+                })
         }
     }
-    
+
     actionClone(resource) {
         // resource should be an object with person_id e.g {person_id: 1}
         const date = (new moment).add(5, 'days')
         return this.taskService.clone(this.getTask(this.action))
-        .then(task => {
-            task.user_id = this.workflow.user_id
-            task.person_id = resource.person_id
-            task.created_by = this.workflow.user_id
-            task.updated_by = this.workflow.user_id
-            task.due_date = date.format(DATEFORMAT)
-            task.is_completed = 0
-            task.status = 1
+            .then(task => {
+                task.user_id = this.workflow.user_id
+                task.person_id = resource.person_id
+                task.created_by = this.workflow.user_id
+                task.updated_by = this.workflow.user_id
+                task.due_date = date.format(DATEFORMAT)
+                task.is_completed = 0
+                task.status = 1
 
-            return Promise.resolve(task)
-        })
-        .then(task => {
-            return this.taskService.add(task)
-        })
-        .then(result => {
-            if(result) {
-                return this.log(resource, 1, 'Task cloned.')
-                .then(() => {
-                    return Promise.resolve(true)
-                })
-            }
-            else {
-                return this.log(resource, 0, 'Task clone failed!')
-                .then(() => {
-                    return Promise.resolve(false)
-                })
-            }
-        })
+                return Promise.resolve(task)
+            })
+            .then(task => {
+                return this.taskService.add(task)
+            })
+            .then(result => {
+                if (result) {
+                    return this.log(resource, 1, 'Task cloned.')
+                        .then(() => {
+                            this.finished()
+                            return Promise.resolve(true)
+                        })
+                } else {
+                    return this.log(resource, 0, 'Task clone failed!')
+                        .then(() => {
+                            this.finished()
+                            return Promise.resolve(false)
+                        })
+                }
+            })
     }
 
     actionAssign(resource) {
-        
+
         //init date with 5 days value in timestamp format, use momentjs
         const date = (new moment).add(5, 'days')
-        
+
         // init empty task object
         let task = {}
-        
+
         // perform getTask from action 
         return this.getTask(this.action)
-        .then(result => {
-            result.user_id = this.workflow.user_id
-            result.updated_by = this.workflow.user_id
-            result.due_date = date.format(DATEFORMAT)
-            result.is_completed = 0
-            result.status = 1
+            .then(result => {
+                result.user_id = this.workflow.user_id
+                result.updated_by = this.workflow.user_id
+                result.due_date = date.format(DATEFORMAT)
+                result.is_completed = 0
+                result.status = 1
 
-            task = result
-            return result
-        })
-        .then(task => {
-            return this.taskService.edit(task)
-            
-        })
-        .then(result => {
-            resource.tableName = this.taskService.tableName
+                task = result
+                return result
+            })
+            .then(task => {
+                return this.taskService.edit(task)
 
-            if(typeof result != 'undefined' && result !== null) {
-                //if result has valid value
-                //log and return true
-                return this.log(resource, 1, 'Task '+task.task_action+' assigned')
-                .then(() => {
-                    return true
-                })
-            }
-            else {
-                return this.log(resource, 0, 'Task assign failed!')
-                .then(() => {
-                    return false
-                })
-            }
-        })
+            })
+            .then(result => {
+                resource.tableName = this.taskService.tableName
+
+                if (typeof result != 'undefined' && result !== null) {
+                    //if result has valid value
+                    //log and return true
+                    return this.log(resource, 1, 'Task ' + task.task_action + ' assigned')
+                        .then(() => {
+                            this.finished()
+                            return true
+                        })
+                } else {
+                    return this.log(resource, 0, 'Task assign failed!')
+                        .then(() => {
+                            this.finished()
+                            return false
+                        })
+                }
+            })
     }
 
     getExecuteParams(resource) {
@@ -222,7 +245,7 @@ class ActionResourcesJob {
         params.push(resource)
         params.push(this.action.value)
 
-        return params
+        return Promise.resolve(params)
     }
 
     getActionResource(resource, resourceService) {
@@ -244,9 +267,9 @@ class ActionResourcesJob {
         // })
 
         return this.setCriteria(target, model, resource, relationMaps)
-        .then(result => {
-            return model.read(result.id)
-        })
+            .then(result => {
+                return model.read(result.id)
+            })
     }
 
     setJoin(target, model, relation) {
@@ -254,18 +277,17 @@ class ActionResourcesJob {
         let onKey
         let onValue
         const relations = {}
-        // {key: val}
-        // {table: {key: val}}
+            // {key: val}
+            // {table: {key: val}}
 
         _.map(relation, (value, key) => {
-            if(typeof value === 'object') {
+            if (typeof value === 'object') {
                 onKey = Object.keys(value)[0]
                 onValue = value[onKey]
                 relations[onKey] = onValue
 
                 model.join(key, relations, 'left')
-            }
-            else {
+            } else {
                 relations[key] = value
                 model.join(target, relations, 'left')
             }
