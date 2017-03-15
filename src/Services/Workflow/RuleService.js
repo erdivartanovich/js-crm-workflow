0bb41f295b23d47eef5783f0752d5f8d92a26595
rafi-isakh <rafiisakh@gmail.com>
2017-03-15 12:54:02 +0700

    refactory-id/kw-crm#149 - add getDependentRule method

--- src/Services/Workflow/RuleService.js
+++ src/Services/Workflow/RuleService.js
@@ -17,19 +17,19 @@ class RuleService extends BaseService {
         return knex.transaction(trx => {
             return trx.from(this.ruleActionName).where('rule_id', rule.id).delete().then(() => {
                 return trx.insert(actions.map(data => {
-                   data.rule_id = rule.id
-                   return data
-              })).into(this.ruleActionName)
+                    data.rule_id = rule.id
+                    return data
+                })).into(this.ruleActionName)
             })
         })
-        
+
     }
 
     getParent(rule) {
         if (rule.parent_id === null) {
             return Promise.resolve(null)
         }
-        
+
         return this.read(rule.parent_id)
     }
 
@@ -49,11 +49,43 @@ class RuleService extends BaseService {
         })
     }
 
+    getDependentRules(workflow, action) {
+        return knex(this.tableName)
+            .where('workflow_id', workflow.id)
+            .whereNotNull('parent_id')
+            .then(result => {
+                console.log('Hello', result)
+                const ids = []
+                result.map(item => {
+                    ids.push(item.id)
+                })
+                return knex(this.ruleActionName)
+                    .whereIn('rule_id', ids)
+                    .where('action_id', action.id)
+            })
+            .then(result => {
+                const rules = []
+                result.map(item => {
+                    rules.push(item.rule_id)
+                })
+                return knex(this.tableName)
+                    .whereIn('id', rules)
+            })
+            .then(result => {
+                const parentIds = []
+                result.map(item => {
+                    parentIds.push(item.parent_id)
+                })
+                return knex(this.tableName)
+                    .whereIn('id', parentIds)
+            })
+    }
+
     // getDependentRules() method using elasticsearch
     getRuleThatHasAction(workflow, action) {
         return knex(this.ruleActionName)
             .where('action_id', action.id)
-            .then( result => {
+            .then(result => {
                 const ids = []
                 result.map(res => {
                     ids.push(res.rule_id)
