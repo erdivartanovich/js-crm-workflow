ffd1110e0b4240dd7411b6613de346a9c040c006
erdivartanovich <erdivartanovich@gmail.com>
2017-03-17 04:32:42 +0700

    refactory-id/kw-crm#156 define function to get instance of tags from db that should contains tag.id

--- src/Services/Task/TaskService.js
+++ src/Services/Task/TaskService.js
@@ -4,139 +4,187 @@
 */
 const knex = require('../../connection')
 const BaseService = require('../BaseService')
+const tagService = require('../Tags/TagService')
+const Promise = require('bluebird')
 
 class TaskService extends BaseService {
   /**
   * declare constructor for Task Service class
   */
-  constructor() {
-    super()
-    this.tableName = 'tasks'
-    this.model = knex(this.tableName)
-  }
+    constructor() {
+        super()
+        this.tableName = 'tasks'
+        this.model = knex(this.tableName)
+        this.tagService = new tagService
+    }
 
   /**
   * additional function: active
   */
-  activate(payload) {
-    return knex(this.tableName)
+    activate(payload) {
+        return knex(this.tableName)
           .where('id', payload['id'])
           .update({
-            status: 1,
-            updated_by: payload['updated_by'],
-            updated_at: this.getNow()
+              status: 1,
+              updated_by: payload['updated_by'],
+              updated_at: this.getNow()
           })
-  }
+    }
 
-  /**
+  /**null
   * additional function: deactivate
   * to
   */
-  deactivate(payload) {
-    return knex(this.tableName)
+    deactivate(payload) {
+        return knex(this.tableName)
           .where('id', payload['id'])
           .update({
-            status: 0,
-            updated_by: payload['updated_by'],
-            updated_at: this.getNow()
+              status: 0,
+              updated_by: payload['updated_by'],
+              updated_at: this.getNow()
           })
-  }
+    }
 
   /**
   * additional function: attachTags
   */
-  attachTags() {
-    // fix me
-  }
+    attachTags(task, user, tags) {
+        const ids = []
+        const arrTag = []
+        const tagModel = knex('tags')
+        const taggablesModel = knex('taggables')
+        
+        
+        // safety check of undefined tags
+        tags = !!tags ? tags : []
+
+        //flatten array of tag objects 
+        tags.map((tag) => {
+            arrTag.push(tag.tag)
+        })
+
+        //define function to get instance of tags from db that should contains tag.id
+        const getTagsRecords = function() {
+            return tagModel.whereIn('tag', arrTag)
+        }
+
+        
+
+        
+        
+
+      // task->tags()->attach(ids); (new TaskObserver)->updated(task); foreach (tags
+      // as tag) {     (new TagObserver)->updated(tag); } return true;
+    }
 
   /**
   * additional function: detachTags
   */
-  detachTags() {
-    // fix me
-  }
+    detachTags() {
+      // fix me
+    }
 
   /**
   * additional function: syncTags
   */
-  syncTags() {
-    // fix me
-  }
+    syncTags() {
+      // fix me
+    }
 
   /**
   * additional function: markCompleted
   */
-  markCompleted(payload) {
-    // fix me
-  }
+    markCompleted(payload) {
+      // fix me
+    }
 
   /**
   * additional function: markNotCompleted
   */
-  markNotCompleted(payload) {
-    return knex(this.tableName)
+    markNotCompleted(payload) {
+        return knex(this.tableName)
           .where('id', payload['id'])
           .update({
-            is_completed: 0,
-            updated_by: payload['updated_by'],
-            updated_at: this.getNow()
+              is_completed: 0,
+              updated_by: payload['updated_by'],
+              updated_at: this.getNow()
           })
-  }
+    }
 
   /**
   * additional function: assignTo
   */
-  assignTo() {
-    // fix me
-  }
+    assignTo() {
+      // fix me
+    }
 
   /**
   * additional function: restore
   */
-  restore() {
-    // fix me
-  }
+    restore() {
+      // fix me
+    }
 
   /**
   * additional function: clone
   */
-  clone(task) {
-      let data = {
-          //build payload object, with property value is null if not defined
-          task_type: task.task_type ? task.task_type : null,
-          task_action: task.task_action ? task.task_action : null,
-          due_date: task.due_date ? task.due_date : null,
-          from_interaction: task.from_interaction ? task.from_interaction : null,
-          reason: task.reason ? task.reason : null,
-          description: task.description ? task.description : null,
-          is_completed: task.is_completed ? task.is_completed : null,
-          is_automated: task.is_automated ? task.is_automated : null,
-          status: task.status ? task.status : null,
-      }
+    clone(task) {
+        let data = {
+            //build payload object, with property value is null if not defined
+            task_type: task.task_type
+              ? task.task_type
+              : null,
+            task_action: task.task_action
+              ? task.task_action
+              : null,
+            due_date: task.due_date
+              ? task.due_date
+              : null,
+            from_interaction: task.from_interaction
+              ? task.from_interaction
+              : null,
+            reason: task.reason
+              ? task.reason
+              : null,
+            description: task.description
+              ? task.description
+              : null,
+            is_completed: task.is_completed
+              ? task.is_completed
+              : null,
+            is_automated: task.is_automated
+              ? task.is_automated
+              : null,
+            status: task.status
+              ? task.status
+              : null
+        }
 
-      return this.model.where(data);
+        return this
+          .model
+          .where(data)
     }
 
   /**
   * additional function: isTaskStarredBy
   */
-  isTaskStarredBy() {
-    // fix me
-  }
+    isTaskStarredBy() {
+      // fix me
+    }
 
   /**
   * additional function: starTaskBy
   */
-  starTaskBy() {
-    // fix me
-  }
+    starTaskBy() {
+      // fix me
+    }
 
   /**
   * additional function: unstarTaskBy
   */
-  unstarTaskBy() {
-    // fix me
-  }
+    unstarTaskBy() {
+      // fix me
+    }
 }
 /**
 * export module
