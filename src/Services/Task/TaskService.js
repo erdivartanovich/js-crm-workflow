19bf8a72082d7700580056b5d283eb5516347806
kris <kristoforusrp@gmail.com>
2017-03-18 00:07:40 +0700

    refactory-id/kw-crm#156 Finishing detachTags method

--- src/Services/Task/TaskService.js
+++ src/Services/Task/TaskService.js
@@ -14,7 +14,11 @@ class TaskService extends BaseService {
     constructor() {
         super()
         this.tableName = 'tasks'
+        this.tableTags = 'tags'
+        this.tableTaggables = 'taggables'
         this.model = knex(this.tableName)
+        this.tagsModel = knex(this.tableTags)
+        this.taggablesModel = knex(this.tableTaggables)
         this.tagService = new tagService
     }
 
@@ -49,10 +53,7 @@ class TaskService extends BaseService {
   * additional function: attachTags
   */
     attachTags(task, user, tags) {
-        const arrTag = []
-        const tagModel = knex('tags')
-        const taggablesModel = knex('taggables')
-        
+        const arrTag = []  
         
         // safety check of undefined tags
         tags = !!tags ? tags : []
@@ -64,7 +65,7 @@ class TaskService extends BaseService {
 
         //define function to get instance of tags from db that should contains tag.id
         const getTagsRecords = function() {
-            return tagModel.whereIn('tag', arrTag)
+            return this.tagsModel.whereIn('tag', arrTag)
         }
 
         //define function to populate set of ids that will be attached into taggables id 
@@ -85,7 +86,7 @@ class TaskService extends BaseService {
         const attachIds = function(ids) {
             let payloads = ids
             return Promise.each(payloads, (payload) => {
-                return taggablesModel.insert(payload)
+                return this.taggablesModel.insert(payload)
             })
         }
        
@@ -104,20 +105,20 @@ class TaskService extends BaseService {
   */
     detachTags(tasks, users, tags) {
         const arrTag = []
-        tags.map((tag) => arrTag.push(tag.tag_id))
+        const newTags = !!tags ? tags : []
 
-       return knex('taggables')
-          .whereIn('tag_id', arrTag)
-          .where({
-              task_id: tasks.id,
-              user_id: users.id
-          })
-          .del()
-          .then((result) => {
-              return result
-          })
-          .catch(err => err)
-            
+        newTags.map((tag) => arrTag.push(tag.tag))
+
+        return this.taggablesModel
+                    .where({ 
+                      user_id: users.id, 
+                      taggable_id: tasks.id,
+                      taggable_type: 'tasks'
+                    })
+                    .whereIn('tag_id', arrTag)
+                    .del()            
+                    .then((result) =>  result)
+                    .catch(err => err)
   }
 
 
@@ -230,5 +231,5 @@ module.exports = TaskService
 
 const tag = new TaskService()
 
-return tag.detachTags({id: 10, tag: 'cold'}, {tasks_id: 2}, [{tag_id: 10, tag:'starred'}])
+return tag.detachTags({id: 10, tasks: 'cold'}, {id: 2}, [{id: 1, tag: 'cold'}, {id: 2, tag: 'important'}])
   .then((result) => console.log(result))
