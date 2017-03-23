eff12196db2b729a5e8fd3cf25025c759b3ff6df
erdivartanovich <erdivartanovich@gmail.com>
2017-03-23 20:26:52 +0700

    refactory-id/kw-crm#166 add find method

--- src/Services/Preference/PreferenceService.js
+++ src/Services/Preference/PreferenceService.js
@@ -3,7 +3,27 @@
 const BaseService = require('../BaseService')
 
 class PreferenceService extends BaseService {
-    
+
+    /**
+     * Find preference model.
+     *
+     * @param preferenceable Preferenceable entity
+     * @param identifier     Key to find
+     * @param type           Preference type
+     *
+     * @return PreferenceInterface
+     */
+
+    constructor() {
+        super()
+        this.model = 'preferences'
+    }
+
+    find(preferenceable, identifier, type) {
+        const preference = {preferenceable, identifier, type};
+        return this.model.where(preference)
+    }    
+
 }
 
 module.exports = PreferenceService
